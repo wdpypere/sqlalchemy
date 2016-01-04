fd281e20dafdacd374814a65efd0e1c1443ea174
Mike Bayer <mike_mp@zzzcomputing.com>
2016-01-04 17:39:31 -0500

    add copyrights

--- lib/sqlalchemy/dialects/mysql/reflection.py
+++ lib/sqlalchemy/dialects/mysql/reflection.py
@@ -1,3 +1,10 @@
+# mysql/reflection.py
+# Copyright (C) 2005-2015 the SQLAlchemy authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of SQLAlchemy and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
 import re
 from ... import log, util
 from ... import types as sqltypes
--- lib/sqlalchemy/dialects/mysql/types.py
+++ lib/sqlalchemy/dialects/mysql/types.py
@@ -1,3 +1,10 @@
+# mysql/types.py
+# Copyright (C) 2005-2015 the SQLAlchemy authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of SQLAlchemy and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
 import datetime
 from ... import exc, util
 from ... import types as sqltypes
